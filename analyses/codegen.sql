d1f5c9f65b5eed1fc82f428710d9c253a73d5254
Elena Kochegarova <elenakochegarova@Elenas-MacBook-Pro.local>
2022-04-20 22:04:48 -0700

    Restructured folders, added docs, tests, and packages

---
+++ analyses/codegen.sql
@@ -0,0 +1,17 @@
+{{ codegen.generate_model_yaml(
+    model_name='stg_orders'
+) }}
+
+
+/*
+Codegen is a package for generating templates:
+- sources.yml
+- model's yaml for schema.yml
+- base model's code
+https://hub.getdbt.com/dbt-labs/codegen/latest/
+*/
+
+
+/*
+ {{ codegen.generate_source('') }}
+*/
